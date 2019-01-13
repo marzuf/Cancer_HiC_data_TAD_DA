dff25c737f4353f44ca8b179d743ff95bc7f73cc
mzuer <z_marie_7@hotmail.com>
2019-01-13 11:23:21 +0100

    13c.01.19

---
+++ find_consensusTADs.R
@@ -0,0 +1,189 @@
+options(scipen = 100)
+
+library(doMC)
+library(foreach)
+
+startTime <- Sys.time()
+
+cat("> START find_consensusTADs.R\n")
+# Rscript find_consensusTADs.R  MCF-7_40kb Caki2_40kb
+
+SSHFS <- FALSE
+
+registerDoMC(ifelse(SSHFS, 2, 40))
+
+setDir <- ifelse(SSHFS, "/media/electron", "")
+
+if(SSHFS) setwd("/media/electron/mnt/etemp/marie/Dixon2018_integrative_data")
+
+source("utils_fct.R")
+
+source("../Dixon2018_integrative_data/wsm_TADconsensus_withCoverage_version2_fct_withAllTxt.R")
+
+
+#*****************************
+
+# as input: should be able to retrieve _final_domains.txt
+# automatically try to retrieve as much chromos as possible
+# MCF-7_40kb/FINAL_DOMAINS/MCF-7_chr2_KR_40kb_final_domains.txt 
+# 
+# otuput should look like
+# <cl1cl2>_40kb/FINAL_DOMAINS/<cl1cl2>_chr15_KR_40kb_final_domains.txt
+
+# command line: all main input folders
+# Rscript find_consensusTADs.R cl1_40kb cl2_40kb cl3_40kb ...
+
+all_inFolders <- c("MCF-7_40kb", "Caki2_40kb")
+
+all_inFolders <- commandArgs(trailingOnly = TRUE)
+stopifnot(file.exists(all_inFolders))
+
+
+folderSuffix <- unique(gsub(".+(_.+?kb)$", "\\1", all_inFolders))
+stopifnot(length(folderSuffix) == 1)
+all_cl <- gsub(folderSuffix, "", all_inFolders)
+consensusClName <- paste0(all_cl, collapse="")
+outFolder <- file.path(paste0(consensusClName, folderSuffix), "FINAL_DOMAINS")
+stopifnot(length(outFolder) == 1)
+
+dir.create(outFolder, recursive=TRUE)
+
+
+#*****************************
+logFile <- file.path(outFolder, paste0(consensusClName, "_consensusTADs.logFile.txt"))
+if(!SSHFS) system(paste0("rm -f ", logFile))
+if(SSHFS) logFile <- ""
+
+# hard-coded settings for the consensus
+txt  <- paste0("*** HARD-CODED PIPELINE SETTINGS:\n")
+printAndLog(txt, logFile)
+domainFolder <- "FINAL_DOMAINS"
+txt  <- paste0("... domainFolder\t=\t", domainFolder, "\n")
+tadfile_suffixPattern <- "_final_domains.txt"
+txt  <- paste0("... tadfile_suffixPattern\t=\t", tadfile_suffixPattern, "\n")
+
+# hard-coded settings for the consensus
+txt  <- paste0("*** HARD-CODED SETTINGS FOR CONSENSUS TAD CALLING ALGORITHM:\n")
+printAndLog(txt, logFile)
+
+set_tolRad <- 80000
+set_conservThresh <- ifelse(length(all_inFolders) == 3, 2/3, 
+                            ifelse(length(all_inFolders) == 2, 1, NA))
+if(is.na(set_conservThresh)) stop("error: check set_conservThresh\n")
+set_weightValue <- NULL
+set_coverageThresh <-  1
+set_tadfileHeader <-  FALSE
+set_chrSize <- NULL
+set_ncpu <- ifelse(SSHFS, 2, 40)
+set_fileAsInput <- TRUE
+set_logFile <- NULL
+
+txt  <- paste0("... set_tolRad\t=\t", set_tolRad, "\n")
+printAndLog(txt, logFile)
+txt  <- paste0("... set_conservThresh\t=\t", set_conservThresh, "\n")
+printAndLog(txt, logFile)
+txt  <- paste0("... set_weightValue\t=\t", set_weightValue, "\n")
+printAndLog(txt, logFile)
+txt  <- paste0("... set_coverageThresh\t=\t", set_coverageThresh, "\n")
+printAndLog(txt, logFile)
+txt  <- paste0("... set_tadfileHeader\t=\t", as.character(set_tadfileHeader), "\n")
+printAndLog(txt, logFile)
+txt  <- paste0("... set_chrSize\t=\t", as.character(set_chrSize), "\n")
+printAndLog(txt, logFile)
+txt  <- paste0("... set_ncpu\t=\t", set_ncpu, "\n")
+printAndLog(txt, logFile)
+txt  <- paste0("... set_fileAsInput\t=\t", set_fileAsInput, "\n")
+printAndLog(txt, logFile)
+txt  <- paste0("... set_logFile\t=\t", as.character(set_logFile), "\n")
+printAndLog(txt, logFile)
+
+txt  <- paste0("*** SETTINGS RETRIEVED FROM COMMAND LINE:\n")
+printAndLog(txt, logFile)
+
+txt  <- paste0("... all_inFolders\t=\t", paste0(all_inFolders, collapse=", "), "\n")
+printAndLog(txt, logFile)
+
+###############################################################################################################
+###############################################################################################################
+###############################################################################################################
+
+domain_inFolders <- file.path(all_inFolders, domainFolder)
+stopifnot(dir.exists(domain_inFolders))
+
+# retrieve for each CL the available domain files (hold in list to retrieve common chromo)
+domain_inFiles <- lapply(domain_inFolders, function(inFolder) {
+  inFiles <- list.files(inFolder, full.names = TRUE, recursive=FALSE, pattern=paste0(tadfile_suffixPattern, "$"))
+  stopifnot(length(inFiles) > 0)
+  inFiles
+})
+stopifnot(length(domain_inFiles) == length(all_cl))
+
+# retrieve normalization (for name of output file)
+normMeth <- unique(sapply( unlist(domain_inFiles), function(dfile) gsub(paste0(".+_chr.+?_(.+?)", folderSuffix, tadfile_suffixPattern), "\\1", basename(dfile))))
+stopifnot(length(normMeth) == 1)
+
+# retrieve for each CL the available chromos
+all_chromos <- lapply(seq_along(domain_inFiles), function(i) {
+  cl_chromos <- unique(gsub(paste0(".+_(chr.+?)_.+", folderSuffix, tadfile_suffixPattern), "\\1", basename(domain_inFiles[[i]])))
+  stopifnot(cl_chromos %in% c(paste0("chr", 1:23), "chrX"))
+  cl_chromos
+})
+
+# retrieve common chromo
+intersectChromo <- Reduce(intersect, all_chromos)
+stopifnot(length(intersectChromo) > 0)
+stopifnot(intersectChromo %in% c(paste0("chr", 1:23), "chrX"))
+
+txt <- paste0("... found common chromo(s):\t", paste0(intersectChromo, collapse=","), "\n")
+printAndLog(txt, logFile)
+
+# intersectChromo <- intersectChromo[1]
+# intersectChromo <- "chrX"
+
+for(chromo in intersectChromo) {
+  
+  cat(paste0("> START chromo = ", chromo, "\n"))
+  
+  chromo_files <- lapply(seq_along(domain_inFiles), function(i) {
+    curr_files <- domain_inFiles[[i]]
+    curr_chr_file <- curr_files[grep(paste0(chromo, "_"), basename(curr_files))]
+    stopifnot(file.exists(curr_chr_file))
+    stopifnot(length(curr_chr_file) == 1)
+    curr_chr_file
+  })
+  
+    domainsDT <- get_consensus_TADs(chromo_files,
+                                    tolRad=set_tolRad,
+                                    conservThresh=set_conservThresh,
+                                    weightValue = set_weightValue,
+                                    coverageThresh = set_coverageThresh,
+                                  tadfileHeader = set_tadfileHeader,
+                                  chrSize=set_chrSize,
+                                  ncpu=set_ncpu,
+                                  fileAsInput=set_fileAsInput,
+                                  logFile=set_logFile)
+  
+    
+  cat("nrow consensus:", nrow(domainsDT), "\n")
+  
+  if(nrow(domainsDT) > 1 ){
+    for(i in 2:nrow(domainsDT)) {
+      # cat(paste0(i, "\t"))
+      stopifnot( domainsDT[i, 2] > domainsDT[i-1, 3]  )
+    }
+  }
+  # <cl1cl2>_40kb/FINAL_DOMAINS/<cl1cl2>_chr15_KR_40kb_final_domains.txt
+  outFile <- file.path(outFolder, paste0(consensusClName, "_", chromo,"_", normMeth, "_", folderSuffix, "_", tadfile_suffixPattern))
+  write.table(domainsDT, file = outFile, sep="\t", quote=F, col.names=F, row.names=F)
+  cat(paste0("... written:\t", outFile, "\n"))  
+}
+ 
+
+######################################################################################
+cat(paste0("... written: ", logFile, "\n"))
+cat("*** DONE\n")
+cat(paste0(startTime, "\n", Sys.time(), "\n"))
+
+
+
+
--- prepFromHiC_readme.txt
+++ prepFromHiC_readme.txt
@@ -3,27 +3,28 @@
 
 0) download the raw data at 10 kb
 [command line: juicer dump extract 3 column format 10 kb matrix]
+# all_chromo_dwd_juicer_hic.sh #
+-> MCF-7/RAW_10kb/MC-7_<chromo>_RAW_10kb.hic.counts
 
-1) 10 kb -> 40 kb raw data using prepFromHiC_aggregCounts.R
+1) 10 kb -> 40 kb raw data using *prepFromHiC_aggregCounts.R*
+all_chromo_hicData_rebinning.sh
+-> MCF-7/RAW_40kb/MC-7_<chromo>_RAW_40kb.hic.counts
 
-1b) aggregate using HiTC and compare the result with cmp_rebinning_custom_HiTC.R
+1b) aggregate using HiTC and compare the result with *cmp_rebinning_custom_HiTC.R*
 
-2) prepare a pre.txt file from the rebinned data to create a hic file using prepFromHic_prepFile.R
+
+
+
+2) prepare a pre.txt file from the rebinned data to create a hic file using *prepFromHic_prepFile.R*
+# all_chromo_prepFile.sh #
+
+MCF-7/PRE_40kb/MC-7_chr21_RAW_40kb.pre
 
 3) juicer command to generate hic file from 40kb data
-+ juicer command to add normalization
+MCF-7/RAW_HIC_INTRA_40kb/MC-7_chr21_intra_40kb.hic  
++ juicer command to extract 40kb KR normalized data
+MCF-7_40kb/NORM_MAT/MC-7_chr21_KR_40kb.hic.matrix
 
-I need the hic file for the 40 kb raw data then for the normalization using juicer
-
-3) KR normalization of the hic that contains 40 kb raw data 
-[command line: juicer addNorm]
+all_chromo_pre2hic2matrix.sh
 
 
-  addNorm <input_HiC_file> [-w genome-wide-resolution] -F
-
-Required:
-
-    <input_HiC_file>: File to normalize; this will delete any previous normalizations Optional:
-    -w <genome-wide resolution>: Smallest resolution to calculate genome-wide resolution; e.g., if 10000, genome-wide normalizations will be calculated for 2.5Mb, 1Mb, 500Kb, 250Kb, 100Kb, 50Kb, 25Kb, and 10Kb but not for 5Kb. Note that genome-wide resolution can be very expensive in terms of memory; this flags allows for a memory/normalization trade-off. If not set or set to 0, no genome-wide resolutions will be calculated
-    -F: Do not calculate normalizations for fragment-delimited matrices
-    -d: For genome-wide normalization, include intra-chromosomal matrices; by default, inter-only matrices are used.
