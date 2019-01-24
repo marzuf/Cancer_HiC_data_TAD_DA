88d0ef2f2ac40b5669969b9b140d1d64392706e2
mzuer <z_marie_7@hotmail.com>
2019-01-24 12:54:55 +0100

    23.01.19

---
+++ plot_lolliTAD.R
@@ -0,0 +1,194 @@
+#!/usr/bin/Rscript
+
+startTime <- Sys.time()
+
+# Rscript plot_lolliTAD.R TCGAcrc_msi_mss ../TAD_DE_pipeline_v2_TopDom/PLOT_LOLLITAD/TCGAcrc_msi_mss chr6_TAD52 chr6_TAD58
+
+my_dataset <- "TCGAcrc_MSI_MSS"
+settingF <- "SETTING_FILES_cleanInput/run_settings_TCGAcrc_msi_mss.R"
+outFold <- "../TAD_DE_pipeline_v2_TopDom/PLOT_LOLLITAD/TCGAcrc_msi_mss"
+
+args <- commandArgs(trailingOnly = T)
+stopifnot(length(args) > 2)
+my_dataset <- args[1] 
+outFold <- args[2] 
+all_TADs <- args[3:length(args)]
+
+caller <- "TopDom"
+
+################  USE THE FOLLOWING FILES FROM PREVIOUS STEPS
+# - script0: rna_rnaseqDT.Rdata
+# - script0: rna_geneList.Rdata
+# - script0: pipeline_geneList.Rdata
+# - script3: all_meanLogFC_TAD.Rdata
+# - script4: all_meanCorr_TAD.Rdata
+# - script7: meanCorr_permDT.Rdata
+# - script11: emp_pval_combined.Rdata
+# - script21: signifTADs.Rdata
+################################################################################
+
+################  OUTPUT
+# - emp_pval_meanCorr.Rdata + plots
+################################################################################
+SSHFS <- F
+setDir <- ifelse(SSHFS, "/media/electron", "")
+
+settingF <- paste0(setDir, "/mnt/ed4/marie/scripts/TAD_DE_pipeline/SETTING_FILES_cleanInput/run_settings_", my_dataset, ".R")
+cat(settingF, "\n")
+stopifnot(file.exists(settingF))
+
+pipScriptDir <- paste0(setDir, "/mnt/ed4/marie/scripts/TAD_DE_pipeline_v2")
+
+script0_name <- "0_prepGeneData"
+script1_name <- "1_runGeneDE"
+script3_name <- "3_runMeanTADLogFC"
+script4_name <- "4_runMeanTADCorr"
+script11_name <- "11_runEmpPvalCombined"
+script21_name <- "21_getSignifFDR"
+script_name <- "22_plotTopCombinedFDR"
+stopifnot(file.exists(paste0(pipScriptDir, "/", script_name, ".R")))
+cat(paste0("> START ", script_name,  "\n"))
+
+
+source(paste0(pipScriptDir, "_",  caller,"/", "main_settings.R"))
+
+#source("run_settings.R")
+source(settingF)
+# settingF = "SETTING_FILES_NOVOOM/run_settings_GSE71119_dediffSM_MFSM.R"
+source(paste0(pipScriptDir, "/", "TAD_DE_utils.R"))
+suppressPackageStartupMessages(library(grid, warn.conflicts = FALSE, quietly = TRUE, verbose = FALSE)) 
+suppressPackageStartupMessages(library(tools, warn.conflicts = FALSE, quietly = TRUE, verbose = FALSE)) 
+
+pipOutFold <- paste0(pipScriptDir, "_",  caller, "/",pipOutFold)
+
+# create the directories
+# curr_outFold <- paste0("PLOT_LOLLITAD_", my_dataset)
+curr_outFold <- outFold
+system(paste0("mkdir -p ", curr_outFold))
+
+
+plotType <- "svg"
+myHeight <- ifelse(plotType == "png", 480, 7)
+myWidth <- ifelse(plotType == "png", 600, 10)
+
+################################****************************************************************************************
+####################################################### PREPARE INPUT
+################################****************************************************************************************
+
+# INPUT DATA
+gene2tadDT <- read.delim(gene2tadDT_file, header=F, col.names = c("entrezID", "chromo", "start", "end", "region"), stringsAsFactors = F)
+gene2tadDT$entrezID <- as.character(gene2tadDT$entrezID)
+
+entrez2symbDT <- read.delim(entrezDT_file, header=T, stringsAsFactors=F)
+entrez2symbDT <- entrez2symbDT[,c("entrezID", "symbol")]
+colnames(entrez2symbDT) <- c("entrezID", "geneName")
+entrez2symbDT$entrezID <- as.character(entrez2symbDT$entrezID)
+
+meanTADlogFC <- eval(parse(text = load(paste0(pipOutFold, "/", script3_name, "/", "all_meanLogFC_TAD.Rdata"))))
+
+########################################################################################
+########### PREPARE THE GENES TO PLOT THE logFC 
+########################################################################################
+samp1 <- eval(parse(text = load(paste0(setDir, "/", sample1_file))))
+samp2 <- eval(parse(text = load(paste0(setDir, "/", sample2_file))))
+
+# UPDATE SELECT THE GENES ACCORDING TO THE SETTINGS PREPARED IN 0_PREPGENEDATA
+rnaseqDT <- eval(parse(text = load(paste0(pipOutFold, "/", script0_name, "/rna_rnaseqDT.Rdata"))))
+initList <- eval(parse(text = load(paste0(pipOutFold, "/", script0_name, "/rna_geneList.Rdata"))))
+geneList <- eval(parse(text = load(paste0(pipOutFold, "/", script0_name, "/pipeline_geneList.Rdata"))))
+
+txt <- paste0(toupper(script_name), "> Start with # genes: ", length(geneList), "/", length(initList), "\n")
+cat(txt)
+# printAndLog(txt, pipLogFile)
+
+rnaseqDT <- rnaseqDT[names(geneList),]    
+stopifnot(all(rownames(rnaseqDT) == names(geneList)))
+stopifnot(!any(duplicated(names(geneList))))
+stopifnot(!any(duplicated(geneList)))
+
+DE_topTable <- eval(parse(text = load(paste0(pipOutFold, "/", script1_name, "/DE_topTable.Rdata"))))
+stopifnot(!any(duplicated(names(geneList))))
+DE_topTable <- DE_topTable[DE_topTable$genes %in% names(geneList),]
+stopifnot(nrow(DE_topTable) > 0)
+
+gene2tadDT <- gene2tadDT[gene2tadDT$entrezID %in% geneList,]
+
+DE_topTable$genes <- unlist(sapply(DE_topTable$genes, function(x) geneList[x]))
+rownames(DE_topTable) <- NULL
+
+# ! duplicated row.names are not allowed !
+dupEntrez <- unique(geneList[duplicated(geneList)])
+geneList <- geneList[! geneList %in% dupEntrez]
+stopifnot(!any(duplicated(geneList)))
+
+DE_topTable <- DE_topTable[!DE_topTable$genes %in% dupEntrez,]
+stopifnot(!any(duplicated(DE_topTable$genes)))
+
+initNrow <- nrow(rnaseqDT)
+rnaseqDT <- rnaseqDT[which(rownames(rnaseqDT) %in% names(geneList)),]
+txt <- paste0(toupper(script_name), "> Discard duplicated symbol, retain: ", nrow(rnaseqDT), "/", initNrow , " genes\n")
+# printAndLog(txt, pipLogFile)
+cat(txt) 
+
+stopifnot(all(rownames(rnaseqDT) == names(geneList)))
+stopifnot(is.numeric(rnaseqDT[1,1]))
+#if(applyVoomAndCPM) {
+if(inputDataType == "raw" | inputDataType == "RSEM") {
+  log2_rnaseqDT <- log2(rnaseqDT + 0.0001) 
+} else{
+  log2_rnaseqDT <- rnaseqDT
+}
+meanExpr <- rowMeans(log2_rnaseqDT, na.rm=T)
+names(meanExpr) <- unlist(sapply(names(meanExpr), function(x) geneList[x]))
+
+################################****************************************************************************************
+####################################################### DO THE PLOTS 
+################################****************************************************************************************
+###################################################################### PLOT LOLLI TAD
+ 
+# retrieve which condition is cond1, i.e. the one that is more expressed when logFC is positive
+geneHighestLogFC <- names(geneList[geneList == DE_topTable$genes[which.max(DE_topTable$logFC)] ])
+
+samp1_vect <- rnaseqDT[geneHighestLogFC,samp1, drop=F]
+stopifnot(dim(samp1_vect) == c(1,length(samp1)))
+samp2_vect <- rnaseqDT[geneHighestLogFC,samp2, drop=F]
+stopifnot(dim(samp2_vect) == c(1,length(samp2)))
+
+plot_cond1 <- ifelse(as.numeric(rowMeans(samp1_vect, na.rm = T)) > as.numeric(rowMeans(samp2_vect, na.rm = T)), cond1, cond2)
+plot_cond2 <- ifelse(as.numeric(rowMeans(samp1_vect, na.rm = T)) > as.numeric(rowMeans(samp2_vect, na.rm = T)), cond2, cond1)
+
+n_topTAD_toplot <- length(all_TADs)
+
+outWidth <- 20
+outHeight <- min(c(7 * n_topTAD_toplot/2, 49))
+
+if(outHeight < 7) outHeight <- 7
+
+vect_plot <- list()
+for(i_plot in 1:n_topTAD_toplot)  {
+  tad_to_plot <- all_TADs[i_plot]
+  vect_plot[[i_plot]] <- plot_lolliTAD(TAD_to_plot = tad_to_plot,
+                                       meanExpr_vect = meanExpr, 
+                                       DE_table = DE_topTable,
+                                       g2t_table = gene2tadDT, 
+                                       id2name_table=entrez2symbDT, 
+                                       geneList = geneList,
+                                       textLeft =  meanTADlogFC[tad_to_plot] > 0,
+                                       orderBy = "logFC", cond1=plot_cond1, cond2=plot_cond2)
+}
+all_plots <- do.call(grid.arrange, c(vect_plot,  list(ncol=2, top=textGrob(paste(my_dataset),
+                                                                           gp=gpar(fontsize=20,font=2)))))
+cat("myHeight =", outHeight, "\n")
+
+outFile <- paste0(curr_outFold, "/", my_dataset, "_", paste0(all_TADs, collapse = "_"), ".", plotType)
+ggsave(filename = outFile, all_plots, width=outWidth, height = outHeight)
+cat("... written: ", outFile, "\n")
+
+
+###################################################################### 
+txt <- paste0(startTime, "\n", Sys.time(), "\n")
+# printAndLog(txt, pipLogFile)
+cat(txt)
+cat(paste0("*** DONE: ", script_name, "\n"))
+
+
